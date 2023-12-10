8c740c9e3bd81bdaeacb7a28e1b2ed03f8b4b0f5
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-12-11 02:55:42 +0500

    done

--- scripts/config.js
+++ scripts/config.js
@@ -2,16 +2,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDOag5XID0IPtXmBObpd1qQZq-vUspjdPQ",
-  authDomain: "bloggers--hub.firebaseapp.com",
-  projectId: "bloggers--hub",
-  storageBucket: "bloggers--hub.appspot.com",
-  messagingSenderId: "92005196041",
-  appId: "1:92005196041:web:c4023d5c33e17ae478f3d8",
+  apiKey: "AIzaSyBYoEpDB4WnDNsHQefGs_qBHKkyz19ArMM",
+  authDomain: "practice-adc61.firebaseapp.com",
+  projectId: "practice-adc61",
+  storageBucket: "practice-adc61.appspot.com",
+  messagingSenderId: "892450035636",
+  appId: "1:892450035636:web:e31731d749d3ca658e84e1",
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
