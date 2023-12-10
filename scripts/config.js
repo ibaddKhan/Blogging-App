a8d7894e580b92fc08598e3b3319e142021a66dd
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-12-11 03:14:10 +0500

    done

--- scripts/config.js
+++ scripts/config.js
@@ -3,12 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyBYoEpDB4WnDNsHQefGs_qBHKkyz19ArMM",
-  authDomain: "practice-adc61.firebaseapp.com",
-  projectId: "practice-adc61",
-  storageBucket: "practice-adc61.appspot.com",
-  messagingSenderId: "892450035636",
-  appId: "1:892450035636:web:e31731d749d3ca658e84e1",
+  apiKey: "AIzaSyDOag5XID0IPtXmBObpd1qQZq-vUspjdPQ",
+  authDomain: "bloggers--hub.firebaseapp.com",
+  databaseURL: "https://bloggers--hub-default-rtdb.firebaseio.com",
+  projectId: "bloggers--hub",
+  storageBucket: "bloggers--hub.appspot.com",
+  messagingSenderId: "92005196041",
+  appId: "1:92005196041:web:c4023d5c33e17ae478f3d8",
 };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
